ac00e7660fad4c2847ee3c74746dd702704f6c25
최민지 <alswlchl@neonexsoft.com>
2022-02-11 15:10:38 +0900

    이메일 제목 수정

--- src/main/java/com/hankook/pg/mail/service/EmailService.java
+++ src/main/java/com/hankook/pg/mail/service/EmailService.java
@@ -1,6 +1,10 @@
 package com.hankook.pg.mail.service;
+
 import com.hankook.pg.mail.entity.Email;
-        import lombok.extern.slf4j.Slf4j;
+import java.beans.ConstructorProperties;
+import java.util.Map;
+import javax.mail.internet.MimeMessage;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
@@ -11,9 +15,6 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
-import javax.mail.internet.MimeMessage;
-import java.beans.ConstructorProperties;
-import java.util.*;
 
 
 
@@ -55,33 +56,34 @@ public class EmailService {
     @Async
     public void SenderMail(Email email, String type) {
 
-        String subject;
+        String subject = email.getTitle();
         String templateName;
         Map<String, Object> tableMap = email.getTableMap();
 
+
         if (type.equals("E01")) { // 회원가입승인
-            subject = "회원가입 승인 안내 메일";
+//            subject = "회원가입 승인 안내 메일";
             templateName = "memberApprove";
         } else if (type.equals("E02")) { // 회원가입 취소
-            subject = "회원가입 취소 안내 메일";
+//            subject = "회원가입 취소 안내 메일";
             templateName = "memberApproveCancel";
         } else if (type.equals("E03")) { // 트랙예약신청승인
             templateName = "trackReservApprove";
-            subject = email.getTitle();
+//            subject = email.getTitle();
         } else if (type.equals("E04")) { // 트랙예약신청 취소/반려
             templateName = "trackReservApproveCancel";
-            subject = email.getTitle();
+//            subject = email.getTitle();
         } else if (type.equals("E05")) { // 관리자 등록 완료
-            subject = "관리자 등록 완료 안내 메일";
+//            subject = "관리자 등록 완료 안내 메일";
             templateName = "adminRegist";
         } else if (type.equals("E06")) { // 임시비밀번호 발송
-            subject = "임시 비밀번호 발송";
+//            subject = "임시 비밀번호 발송";
             templateName = "tempPass";
         } else if (type.equals("E07")) { // 부대시설 예약 신청/승인
             templateName = "shopReservApprove";
-            subject = "임시 비밀번호 발송";
+//            subject = "임시 비밀번호 발송";
         } else if (type.equals("E08")) { // 부대시설 예약 취소/반려
-            subject = "임시 비밀번호 발송";
+//            subject = "임시 비밀번호 발송";
             templateName = "shopReservApproveCancel";
         } else { // 어느것도아님
             subject = "ERROR 안내";
