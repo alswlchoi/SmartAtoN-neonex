45735b321c8e261156a4ab10f314891109453643
shinhee <shinhee@neonexsoft.com>
2022-03-16 23:45:15 +0900

    운전자 등급 Dto 추가

---
+++ src/main/java/com/hankook/pg/content/admin/driver/dto/DriverLevelDto.java
@@ -0,0 +1,9 @@
+package com.hankook.pg.content.admin.driver.dto;
+
+import lombok.Data;
+
+@Data
+public class DriverLevelDto {
+	private String dLevel;					//운전자레벨코드
+	private String dLevelName;				//운전자레벨이름
+}
